c6b822424e7e423e6f67dcc3e656a90af06dd0bc
unknown <limalimarenatojr@gmail.com>
2025-11-18 13:29:25 +0800

    Removed footer

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -121,20 +121,6 @@ const Sidebar = ({ currentView, onNavigate, darkMode }) => {
           );
         })}
       </nav>
-
-      {/* Footer Info */}
-      <div className={`absolute bottom-0 left-0 right-0 p-4 border-t ${
-        darkMode ? 'border-gray-700 bg-gray-800/50' : 'border-gray-200 bg-white/50'
-      }`}>
-        <div className={`flex items-center space-x-2 text-xs ${
-          darkMode ? 'text-gray-400' : 'text-gray-500'
-        }`}>
-          <svg className="w-4 h-4 text-red-600" fill="currentColor" viewBox="0 0 20 20">
-            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-          </svg>
-          <span>Blockchain enabled</span>
-        </div>
-      </div>
     </aside>
   );
 };
